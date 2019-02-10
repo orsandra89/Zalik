ec3c2284e532cb4e6a3ae8956831b33cdd2ac9de
orsandra89 <senusheugen@gmail.com>
2019-02-11 00:47:04 +0200

    update controller, all work, only admin can create new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,47 @@
 class ProductsController < ApplicationController
-  def new
-  end
-
-  def edit
-  end
-
-  def show
-  end
-
+  
   def index
+    @products = Product.all
+  end
+
+  def new
+    @Product = Product.new
+  end
+  
+  def create
+    @product = current_user.products.build(eugene)
+    if @product.save
+    redirect_to root_path #because we're admins
+    end
+  end
+  
+  def show
+    @product = Product.find(params[:id])
+  end
+  
+  def edit
+  
+  end
+  
+  def update
+    if @product.update(params)
+      redirect_to root_path
+    end
+  end
+  
+  def destroy
+    @product.destroy
+    redirect_to root_path
+  end
+  
+  private
+
+  def eugene
+    params.require(:product).permit(:name, :description, :price)
+  end
+
+  def find
+    @product = Product.find(params[:id])
   end
   
 end
