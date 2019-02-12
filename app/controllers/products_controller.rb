3ff7e22a97c12772458c14766d88733c9487bc7e
orsandra89 <senusheugen@gmail.com>
2019-02-12 15:39:29 +0200

    update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,44 +1,16 @@
 class ProductsController < ApplicationController
-  
+   before_action :find, only: [:show]
   def index
     @products = Product.all
   end
 
-  def new
-    @Product = Product.new
-  end
-  
-  def create
-    @product = current_user.products.build(eugene)
-    if @product.save
-    redirect_to root_path #because we're admins
-    end
-  end
   
   def show
     @product = Product.find(params[:id])
   end
   
-  def edit
-  
-  end
-  
-  def update
-    if @product.update(params)
-      redirect_to root_path
-    end
-  end
-  
-  def destroy
-    @product.destroy
-    redirect_to root_path
-  end
   
   private
-
-  def eugene
-    params.require(:product).permit(:name, :description, :price, photos: [])
-  end
 
   def find
     @product = Product.find(params[:id])
